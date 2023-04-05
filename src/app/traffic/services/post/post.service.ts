cdd590c71daf103a4d742906cbf26c9542fa6550
dejw7n <dejw7n@gmail.com>
2023-04-05 18:32:16 +0200

    Remove getPriorities and getCenters

--- src/app/traffic/services/post/post.service.ts
+++ src/app/traffic/services/post/post.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpRequest } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { environment } from "../../../../environments/environment";
 import { UserService } from "../user/user.service";
@@ -8,36 +8,26 @@ import { UserService } from "../user/user.service";
 	providedIn: "root",
 })
 export class PostService {
-	private apiURl = environment.API_URL;
+	private postApiUrl = environment.API_URL + "/Post/";
+	private fileApiUrl = environment.API_URL + "/FileApi/";
+	private headers = { "content-type": "application/json" };
 
 	constructor(private userService: UserService, private http: HttpClient) {}
 
-	headers = { "content-type": "application/json" };
-
 	getAllPosts(): Observable<any> {
-		return this.http.get(this.apiURl + "/PostApi/getAllPosts", {});
+		return this.http.get(this.postApiUrl + "getAllPosts", {});
 	}
 	getPost(postId: number): Observable<any> {
-		return this.http.get(this.apiURl + "/PostApi/getPost/" + postId, {});
+		return this.http.get(this.postApiUrl + "getPost/" + postId, {});
 	}
 	getFilesByPostId(postId: number): Observable<any> {
-		return this.http.get(this.apiURl + "/FileApi/getFilesByPostId?postId=" + postId, {});
-	}
-	getPriorities(): Observable<any> {
-		return this.http.post(this.apiURl + "/PostApi/getPriorities", {}, { headers: this.headers });
-	}
-	getCenters(): Observable<any> {
-		return this.http.post(this.apiURl + "/PostApi/getCenters", {}, { headers: this.headers });
+		return this.http.get(this.fileApiUrl + "getFilesByPostId?postId=" + postId, {});
 	}
 	editPost(postId: number, postData: any) {
-		postData.id = postId;
 		postData.userId = this.userService.getData().id;
-		this.http.post(this.apiURl + "/PostApi/editPost", postData).subscribe();
+		this.http.put(this.postApiUrl + postId, postData).subscribe();
 	}
 	deletePost(postId: number) {
-		const formData = {
-			postId: postId,
-		};
-		this.http.post(this.apiURl + "/PostApi/deletePost", formData).subscribe();
+		this.http.delete(this.postApiUrl + postId).subscribe();
 	}
 }
