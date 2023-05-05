a5526aee5cc3a6a025d446ea95c39cf35e07be39
dejw7n <dejw7n@gmail.com>
2023-05-05 09:12:27 +0200

    Revert "Create board module"
    This reverts commit 2a144b3007eafbbb8bf4f3202fdd1b384758eb50.

--- src/app/staff/staff.module.ts
+++ src/app/staff/staff.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DashboardModule } from "./pages/dashboard/dashboard.module";
 import { UsersModule } from "./pages/users/users.module";
-import { StaffRoutingModule } from "./staff-routing.module";
-import { StaffComponent } from "./staff.component";
 
 @NgModule({
-	declarations: [StaffComponent],
-	imports: [StaffRoutingModule, CommonModule, DashboardModule, UsersModule],
-	bootstrap: [StaffComponent],
+	declarations: [],
+	imports: [CommonModule, DashboardModule, UsersModule],
+	exports: [],
 })
 export class StaffModule {}
